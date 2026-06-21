93bf196569f3b0bec2e572506681b6e3fa211c90
diffth <diffth@nate.com>
2026-06-21 14:39:49 +0900

    0621 10

--- fastapi_basic/main.py
+++ fastapi_basic/main.py
@@ -2,12 +2,21 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-
+# http://127.0.0.1:8000/
 @app.get("/")
 def read_root():
-    return {"Hello": "World"}
+    word = "안녕"
+    # 비지니스 로직 처리
+    # DB 조회
+    # AI와 통신한 결과
+    return {"Hello": word}
+    # return {"Hello": "World"}
 
-
+# 경로 매개 변수
+# http://127.0.0.1:8000/items/1
+# http://127.0.0.1:8000/items/2
+# http://127.0.0.1:8000/items/3
+# http://127.0.0.1:8000/items/4
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: str | None = None):
     return {"item_id": item_id, "q": q}

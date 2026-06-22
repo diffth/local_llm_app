5537af490df376d1b8bb26cb5b8ca8fd309d0e3b
diffth <diffth@nate.com>
2026-06-22 13:56:12 +0900

    0622 06

--- react_basic/src/App.jsx
+++ react_basic/src/App.jsx
@@ -1,12 +1,14 @@
-import { useState } from 'react'
+import Header from "./components/Header";
+import Greeting from "./components/Greeting"
+
 
 function App() {
-
   return (
-    <>
-      
-    </>
-  )
+    <div>
+      <Header />
+      <Greeting />
+    </div>
+  );
 }
 
-export default App
+export default App;
